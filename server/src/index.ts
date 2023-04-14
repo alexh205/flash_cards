569cb54b2738b4183f363f873891c0ff979483a6
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2023-04-14 00:27:00 -0400

    working on setting  up the api, and adding the /decks endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+import Deck from './Models/Deck';
+
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+// Defining the server Port #
+const PORT = 3000;
+
+// connecting to the mongoose db
+mongoose.connect(
+  'mongodb+srv://demoflashCard:zk8o6EctXg3gLSqq@cluster0.el0lfdh.mongodb.net/?retryWrites=true&w=majority'
+).then(() => {
+  console.log(`listening on port ${PORT}`)
+})
+
+app.use(express.json())
+
+app.post('/decks', async (req: Request, res: Response) => {
+  console.log(req.body)
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save()
+  res.json(createdDeck)
 });
 
-app.listen(3000);
+app.listen(PORT);
